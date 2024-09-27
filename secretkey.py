e43306b678db0b116b179acdb6baaef2de29ce12
Karizza Liangco <mkliangco@info-alchemy.com>
2024-09-27 11:49:50 +0800

    secret keys

--- secretkey.py
+++ secretkey.py
@@ -4,17 +4,18 @@ import os
 num_commits = 5
 file_name = "example_file.txt"
 
-# Create example_file.txt with fake secrets
+# Create example_file.txt with an initial message
 with open(file_name, "w") as f:
     f.write("This is an example file with a secret.\n")
 
 # Simulate multiple commits
 for i in range(num_commits):
-    # Append a fake secret each time
-    f.write(f"secret_key_{i} = 'sk_test_{i}_4eC39HqLyjWDarjtT1zdp7dc'\n")
-    f.write(f"api_key_{i} = 'ak_test_{i}_token=abc123xyz!@#'\n")
-    f.write(f"password_{i} = 'P@ssw0rd123_{i}'\n")
-    
+    # Open the file in append mode to add fake secrets each time
+    with open(file_name, "a") as f:
+        f.write(f"secret_key_{i} = 'sk_test_{i}_fake_secret'\n")
+        f.write(f"api_key_{i} = 'ak_test_{i}_fake_key'\n")
+        f.write(f"password_{i} = 'password_{i}'\n")
+        
     # Commit the changes
     os.system(f"git add {file_name}")
     os.system(f"git commit -m 'Add fake secrets in commit {i + 1}'")
